ba8c52d0a9d6d842ca18b2d14230cdabebcaa9ce
oscar Velez <ovelez_24@hotmail.com>
2022-07-22 10:14:15 -0500

    cambio 5

---
+++ mymoneybox.js
@@ -0,0 +1,13 @@
+function moneyBox(){
+    let saveCoins = 0;
+    function countCoins(coins){
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+}
+
+const myMoneyBox = moneyBox();
+myMoneyBox(5);
+myMoneyBox(5);
+myMoneyBox(15);
